4023042f82d7c00bd3d27ec4df7ac5a2a523c055
akhil ps <49308257+akhilpsin@users.noreply.github.com>
2020-03-19 22:20:41 +0530

    Update settings.py

--- touch_of_care/settings.py
+++ touch_of_care/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'akhilpsinbox@gmail.com'
-EMAIL_HOST_PASSWORD = 'vineeth@123'
+EMAIL_HOST_USER = 'your@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
