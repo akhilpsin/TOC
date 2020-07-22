8903a9d3c32c15f2ed989b9e0d6757ef3ada937d
akhil ps <akhilpsinbox@gmail.com>
2020-07-23 00:40:57 +0530

    preping for heruko

--- proj/settings.py
+++ proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c9-r!y=d(%&u#i6r-d+xae(n=(j^is#(+&za@6)#-_7)ks70im'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['touchofcare.herokuapp.com','127.0.0.1']
 
 
 # Application definition
