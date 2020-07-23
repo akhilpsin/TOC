1eb96d53228f7bd2efb599b39d4d61558579fae0
akhil ps <akhilpsinbox@gmail.com>
2020-07-23 10:44:28 +0530

    added more files

--- proj/settings.py
+++ proj/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/astaticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
